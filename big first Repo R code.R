bcdac2fe58fbd2fab8b62560948c34ef17eed8ea
zaaz1111 <zaahir11@gmail.com>
2023-06-20 18:21:16 -0400

    Fixed the mutate code and did it for all columns that are relevant to standardizing body condiditon (I think)

--- big first Repo R code.R
+++ big first Repo R code.R
@@ -1,2 +1,16 @@
 #This is my code for Izzy
+shelf(tidyverse, readxl, here)
 
+sealdata<-read_xlsx(here('raw data/seal sanctuary data.xlsx'),sheet=1,col_types = 
+                      c('guess','guess','guess','date','guess','numeric','numeric','numeric',
+                        'numeric','numeric','numeric','numeric','guess'))%>%
+  mutate(ax_sl_std=sealdata$Width_ax_cm/sealdata$`Standard _length_cm`,
+         fprint_sl_std=sealdata$Footprint/sealdata$`Standard _length_cm`,
+         vol_sl_std=sealdata$Volume_m3/sealdata$`Standard _length_cm`,
+         so_no_head_sl_std=sealdata$Volume_no_head_m3/sealdata$`Standard _length_cm`)
+
+drone<-sealdata%>%
+  subset(subset=sealdata$File_type=="Drone")
+
+scan<-sealdata%>%
+  subset(subset=sealdata$File_type=="Scan")
